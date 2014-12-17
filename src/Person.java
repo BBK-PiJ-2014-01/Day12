6446cfff669318568e340bd6f1bcb533b75a9a2d
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-17 03:12:11 +0000

    Exercise 3 - code complete

--- src/Person.java
+++ src/Person.java
@@ -8,12 +8,10 @@ public class Person {
         String nextInitial = "";
         String[] words = fullName.split(" ");
         for (int i = 0; i < words.length; i++) {
-            System.out.println(words[i]);
             if (words[i].equals("")) {
                 result = result + "";
             } else {
                 nextInitial = "" + words[i].charAt(0);
-                System.out.println(nextInitial);
                 result = result + nextInitial.toUpperCase();
             }
         }
